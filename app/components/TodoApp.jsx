385d111103f0ac0882a3966c11ecda41d49ec38b
Sasha <lyshirokov613@gmail.com>
2016-12-22 15:46:05 +0300

    Add todos reducer and tests

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -22,7 +22,7 @@ var TodoApp = React.createClass({
 		var updatedTodos = this.state.todos.map((todo) => {
 			if (todo.id === id) {
 				todo.completed = !todo.completed;
-				todo.completedAt = todo.completed ? moment().unix() : undefined
+				todo.completedAt = todo.completed ? moment().unix() : undefined;
 			}
 			return todo;
 		});
